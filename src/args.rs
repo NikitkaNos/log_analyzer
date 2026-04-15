75fdbaf6d3d9585ab3ba6739776af2d2e159ee6e
nos <nikibos1992@gmail.com>
2026-04-15 16:46:04 +0300

    add test args

--- src/args.rs
+++ src/args.rs
@@ -4,6 +4,7 @@ mod tests {
 
     #[test]
     fn test_args_parse_with_two_arguments() {
+        // Симулируем аргументы: ["program_name", "log.txt", "ERROR"]
         let args = vec![
             "log_analyzer".to_string(),
             "log.txt".to_string(),
@@ -13,5 +14,14 @@ mod tests {
         let result = Args::parse_from(args);
 
         assert!(result.is_ok());
+        let args = result.unwrap();
+        assert_eq!(args.file_path, std::path::PathBuf::from("log.txt"));
+        assert_eq!(args.pattern, "ERROR");
     }
+}
+use std::path::PathBuf;
+#[derive(Debug, CLone, PartialEq)]
+pub struct Args {
+    pub file_path: PathBuf,
+    pub pattern: String,
 }
